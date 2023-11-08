bd861ed39267e1e4df534bdace043fd073a3b3ba
Rosshoven <Michaeledwardross@gmail.com>
2023-11-08 11:50:05 -0600

    TEST for git, arrow function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 let win;
 
-app.on('ready', function () {
+app.on('ready', () => {
   win = new BrowserWindow({
     width: 800,
     height: 600,
