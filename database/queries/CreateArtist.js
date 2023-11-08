97ca228faf8f6d912fc303befc89887b878fac1b
Rosshoven <Michaeledwardross@gmail.com>
2023-11-08 14:35:45 -0600

    find and create queries written

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -1,9 +1,16 @@
 const Artist = require('../models/artist');
 
 /**
- * Finds a single artist in the artist collection.
+ *  Adds a single artist in the artist collection.
  * @param {object} artistProps - Object containing a name, age, yearsActive, and genre
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+    // const { name, age, yearsActive, genre } = artistProps;
+    // const newArtist = new Artist({ name, age, yearsActive, genre });
+        // artistProps is already recongized as a full object. No destructuring
+
+    const newArtist = new Artist(artistProps);
+    return newArtist.save();
 };
+
